f261761b9ff0c426a25d94d64efd88f3ca4e8e3d
JaydevSR <jaydevsr7701@gmail.com>
2020-07-31 11:52:18 +0530

    fix prime-factors

--- julia/prime-factors/prime-factors.jl
+++ julia/prime-factors/prime-factors.jl
@@ -2,10 +2,10 @@ function prime_factors(number::Real)
     factors = []
     max_factor = number
     factor = 2
-    while number > 1 & factor < max_factor
+    while number > 1 && factor < max_factor
         while number % factor == 0
             number /= factor
-            append!(factors, factor)
+            push!(factors, factor)
             max_factor = sqrt(number)
         end
         factor += 1
