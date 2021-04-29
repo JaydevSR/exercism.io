82c89c723d7e25a420bded476229411d88738ebe
Jaydev Singh Rao <jaydevsr7701@gmail.com>
2021-04-29 20:14:04 +0530

    edit python word-count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,9 +1,7 @@
 import re
+from collections import Counter
 
 def count_words(sentence: str):
     sentence = sentence.lower()
     words = map(lambda x: x.strip("'"), filter(None, re.split("[^a-zA-Z0-9']", sentence)))
-    word_dict = dict()
-    for word in words:
-        word_dict[word] = word_dict.get(word, 0) + 1
-    return word_dict
+    return dict(Counter(words))
